3cb339ebe792c4f5c4420a5c797fece460a4f996
ryanjeffrey <73365865+ryanjeffrey@users.noreply.github.com>
2020-12-26 18:02:12 -0600

    Passed a reference to the toggleFilter method

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Filters from "./Filters";
 
 class App extends Component {
   state = { selectedFilters: [] };
-  toggleFilter = clickedFilterKey => {
+  toggleFilter = (clickedFilterKey) => {
     var newFilters;
     // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes to see how the .includes array method works
     var alreadySelected = this.state.selectedFilters.includes(clickedFilterKey);
     if (alreadySelected) {
       // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter to see how the .filter array method works
       newFilters = this.state.selectedFilters.filter(
-        selectedFilter => selectedFilter !== clickedFilterKey
+        (selectedFilter) => selectedFilter !== clickedFilterKey
       );
     } else {
       // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat to see how the .concat array method works
@@ -22,7 +22,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Filters selectedFilters={this.state.selectedFilters} />
+        <Filters
+          selectedFilters={this.state.selectedFilters}
+          toggleFilter={this.toggleFilter}
+        />
         <Hotels selectedFilters={this.state.selectedFilters} />
       </div>
     );
--- src/Filters/index.js
+++ src/Filters/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import Filter from "./Filter.js";
 import { filterOptions } from "../data.js";
 
-var FiltersList = props => {
+var FiltersList = (props) => {
   /* notice how we've moved our callback function 
   inside of the FiltersList function so that we can access props
   in our definition of the renderFilter callback function */
-  var renderFilter = filter => {
+  var renderFilter = (filter) => {
     // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes to see how the .includes array method works
     var isChecked = props.selectedFilters.includes(filter.key);
-    return <Filter key={filter.key} filter={filter} isChecked={isChecked} />;
+    return (
+      <Filter
+        key={filter.key}
+        filter={filter}
+        isChecked={isChecked}
+        toggleFilter={props.toggleFilter}
+      />
+    );
   };
   var filterListElements = filterOptions.map(renderFilter);
   return (
